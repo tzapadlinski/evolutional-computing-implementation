5b79bc791a0ccb88d3389b846e88062202db099d
KarolRogulski <karol.rogulski@vp.pl>
2024-11-03 22:16:26 +0100

    Added functions

--- app/evolutionary/function.py
+++ app/evolutionary/function.py
@@ -1,6 +1,23 @@
+import benchmark_functions as bf
+import opfunu
+
 class Function:
-    def __init__(self, num_vars):
-        self.num_vars = num_vars
+    def __init__(self, num_vars, function_name):
+        self.function_name = function_name
+
+        if function_name == 'Griewank':
+            self.func = bf.Schwefel(n_dimensions=num_vars)
+        elif function_name == 'Cigar':
+            funcs = opfunu.get_functions_by_classname(function_name)
+            self.func = funcs[0](ndim=num_vars)
 
     def fit(self, x):
-        return x
+        if self.function_name == 'Griewank':
+            result = self.func(x)
+        elif self.function_name == 'Cigar':
+            result = self.func.evaluate(x)
+        return result
+    
+    @staticmethod
+    def get_available_functions():
+        return ['Griewank', 'Cigar']
