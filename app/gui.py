14a7131f997a360f64e3bdb986d4398c7c79e1f3
KarolRogulski <karol.rogulski@vp.pl>
2024-11-08 20:39:34 +0100

    Gui

--- app/gui.py
+++ app/gui.py
@@ -26,72 +26,76 @@ class GeneticAlgorithmGUI:
         self.population_size_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
         self.population_size_entry.grid(row=0, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Number of Variables:", font=("Helvetica", 11)).grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Number of bit per variable:", font=("Helvetica", 11)).grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        self.gene_size_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
+        self.gene_size_entry.grid(row=1, column=1, padx=5, pady=5)
+
+        ttk.Label(input_frame, text="Number of Variables:", font=("Helvetica", 11)).grid(row=2, column=0, padx=5, pady=5, sticky="w")
         self.num_variables_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
-        self.num_variables_entry.grid(row=1, column=1, padx=5, pady=5)
+        self.num_variables_entry.grid(row=2, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Number of Epochs:", font=("Helvetica", 11)).grid(row=2, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Number of Epochs:", font=("Helvetica", 11)).grid(row=3, column=0, padx=5, pady=5, sticky="w")
         self.num_epochs_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
-        self.num_epochs_entry.grid(row=2, column=1, padx=5, pady=5)
+        self.num_epochs_entry.grid(row=3, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Beginning of Range:", font=("Helvetica", 11)).grid(row=3, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Beginning of Range:", font=("Helvetica", 11)).grid(row=4, column=0, padx=5, pady=5, sticky="w")
         self.begin_range_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
-        self.begin_range_entry.grid(row=3, column=1, padx=5, pady=5)
+        self.begin_range_entry.grid(row=4, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="End of Range:", font=("Helvetica", 11)).grid(row=4, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="End of Range:", font=("Helvetica", 11)).grid(row=5, column=0, padx=5, pady=5, sticky="w")
         self.end_range_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
-        self.end_range_entry.grid(row=4, column=1, padx=5, pady=5)
+        self.end_range_entry.grid(row=5, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Selection Method:", font=("Helvetica", 11)).grid(row=5, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Selection Method:", font=("Helvetica", 11)).grid(row=6, column=0, padx=5, pady=5, sticky="w")
         self.selection_method_var = tk.StringVar(value="roulette")
         self.selection_method_dropdown = ttk.Combobox(input_frame, textvariable=self.selection_method_var, font=("Helvetica", 11), state="readonly")
         self.selection_method_dropdown['values'] = ("roulette", "tournament", "best")
-        self.selection_method_dropdown.grid(row=5, column=1, padx=5, pady=5)
+        self.selection_method_dropdown.grid(row=6, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Elite Strategy Amount:", font=("Helvetica", 11)).grid(row=6, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Elite Strategy Amount:", font=("Helvetica", 11)).grid(row=7, column=0, padx=5, pady=5, sticky="w")
         self.elite_strategy_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
-        self.elite_strategy_entry.grid(row=6, column=1, padx=5, pady=5)
+        self.elite_strategy_entry.grid(row=7, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Cross Probability:", font=("Helvetica", 11)).grid(row=7, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Cross Probability:", font=("Helvetica", 11)).grid(row=8, column=0, padx=5, pady=5, sticky="w")
         self.cross_prob_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
-        self.cross_prob_entry.grid(row=7, column=1, padx=5, pady=5)
+        self.cross_prob_entry.grid(row=8, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Mutation Probability:", font=("Helvetica", 11)).grid(row=8, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Mutation Probability:", font=("Helvetica", 11)).grid(row=9, column=0, padx=5, pady=5, sticky="w")
         self.mutation_prob_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
-        self.mutation_prob_entry.grid(row=8, column=1, padx=5, pady=5)
+        self.mutation_prob_entry.grid(row=9, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Inversion Probability:", font=("Helvetica", 11)).grid(row=9, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Inversion Probability:", font=("Helvetica", 11)).grid(row=10, column=0, padx=5, pady=5, sticky="w")
         self.inversion_prob_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
-        self.inversion_prob_entry.grid(row=9, column=1, padx=5, pady=5)
+        self.inversion_prob_entry.grid(row=10, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Uniform Crossover Probability:", font=("Helvetica", 11)).grid(row=10, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Uniform Crossover Probability:", font=("Helvetica", 11)).grid(row=11, column=0, padx=5, pady=5, sticky="w")
         self.uniform_crossover_prob_entry = ttk.Entry(input_frame, font=("Helvetica", 11), width=25)
-        self.uniform_crossover_prob_entry.grid(row=10, column=1, padx=5, pady=5)
+        self.uniform_crossover_prob_entry.grid(row=11, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Cross Method:", font=("Helvetica", 11)).grid(row=11, column=0, padx=5, pady=5, sticky="w")
-        self.cross_method_var = tk.StringVar(value="one-point")
+        ttk.Label(input_frame, text="Cross Method:", font=("Helvetica", 11)).grid(row=12, column=0, padx=5, pady=5, sticky="w")
+        self.cross_method_var = tk.StringVar(value="single")
         self.cross_method_dropdown = ttk.Combobox(input_frame, textvariable=self.cross_method_var, font=("Helvetica", 11), state="readonly")
-        self.cross_method_dropdown['values'] = ("one-point", "two-point", "three-point", "homo")
-        self.cross_method_dropdown.grid(row=11, column=1, padx=5, pady=5)
+        self.cross_method_dropdown['values'] = ("single", "double", "uniform", "seeded")
+        self.cross_method_dropdown.grid(row=12, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Mutation Method:", font=("Helvetica", 11)).grid(row=12, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Mutation Method:", font=("Helvetica", 11)).grid(row=13, column=0, padx=5, pady=5, sticky="w")
         self.mutation_method_var = tk.StringVar(value="one-point")
         self.mutation_method_dropdown = ttk.Combobox(input_frame, textvariable=self.mutation_method_var, font=("Helvetica", 11), state="readonly")
         self.mutation_method_dropdown['values'] = ("one-point", "two-point")
-        self.mutation_method_dropdown.grid(row=12, column=1, padx=5, pady=5)
+        self.mutation_method_dropdown.grid(row=13, column=1, padx=5, pady=5)
 
         available_functions = Function.get_available_functions()
-        ttk.Label(input_frame, text="Function to Optimize:", font=("Helvetica", 11)).grid(row=13, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Function to Optimize:", font=("Helvetica", 11)).grid(row=14, column=0, padx=5, pady=5, sticky="w")
         self.function_var = tk.StringVar(value=available_functions[0])
         self.function_dropdown = ttk.Combobox(input_frame, textvariable=self.function_var, font=("Helvetica", 11), state="readonly")
         self.function_dropdown['values'] = available_functions
-        self.function_dropdown.grid(row=13, column=1, padx=5, pady=5)
+        self.function_dropdown.grid(row=14, column=1, padx=5, pady=5)
 
-        ttk.Label(input_frame, text="Optimization Mode:", font=("Helvetica", 11)).grid(row=14, column=0, padx=5, pady=5, sticky="w")
+        ttk.Label(input_frame, text="Optimization Mode:", font=("Helvetica", 11)).grid(row=15, column=0, padx=5, pady=5, sticky="w")
         self.optimization_mode_var = tk.StringVar(value="MAX")
         self.optimization_mode_dropdown = ttk.Combobox(input_frame, textvariable=self.optimization_mode_var, font=("Helvetica", 11), state="readonly")
         self.optimization_mode_dropdown['values'] = ("MAX", "MIN")
-        self.optimization_mode_dropdown.grid(row=14, column=1, padx=5, pady=5)
+        self.optimization_mode_dropdown.grid(row=15, column=1, padx=5, pady=5)
 
         run_button = ttk.Button(self.root, text="Run Algorithm", command=self.run_algorithm, style="Accent.TButton")
         run_button.pack(pady=15)
@@ -127,6 +131,7 @@ class GeneticAlgorithmGUI:
         self.output_text.delete(1.0, tk.END)
 
         population_size = self.validate_input("Population Size", self.population_size_entry.get(), int)
+        gene_size = self.validate_input("Number of bit per variable:", self.gene_size_entry.get(), int)
         num_variables = self.validate_input("Number of Variables", self.num_variables_entry.get(), int)
         num_epochs = self.validate_input("Number of Epochs", self.num_epochs_entry.get(), int)
         begin_range = self.validate_input("Begin Range", self.begin_range_entry.get(), float)
@@ -143,7 +148,7 @@ class GeneticAlgorithmGUI:
         function = self.function_var.get()
         optimization_mode = self.optimization_mode_var.get()
 
-        if None in [population_size, num_variables, num_epochs, begin_range, end_range, elite_strategy_amount,
+        if None in [population_size, gene_size, num_variables, num_epochs, begin_range, end_range, elite_strategy_amount,
                     cross_prob, mutation_prob, inversion_prob, uniform_crossover]:
             return 
 
@@ -170,14 +175,27 @@ class GeneticAlgorithmGUI:
             
         alg.run()
 
-        result = "[Algorithm Result]"
+        # result = alg.run()
+        result = 'Test'
 
         execution_time = time.time() - start_time
 
-        self.output_text.insert(tk.END, f"Selection Method: {selection_method}\n"
-                                        f"Cross Method: {cross_method}\n"
-                                        f"Mutation Method: {mutation_method}\n"
-                                        f"Function: {function}\n"
-                                        f"Optimization Mode: {optimization_mode}\n"
-                                        f"Execution Time: {execution_time:.4f} seconds\n"
-                                        f"Result: {result}")
+        self.output_text.insert(tk.END,
+                                f"Population Size: {population_size}\n"
+                                f"Number of Bits per Variable: {gene_size}\n"
+                                f"Number of Variables: {num_variables}\n"
+                                f"Number of Epochs: {num_epochs}\n"
+                                f"Beginning of Range: {begin_range}\n"
+                                f"End of Range: {end_range}\n"
+                                f"Selection Method: {selection_method}\n"
+                                f"Elite Strategy Amount: {elite_strategy_amount}\n"
+                                f"Cross Probability: {cross_prob}\n"
+                                f"Mutation Probability: {mutation_prob}\n"
+                                f"Inversion Probability: {inversion_prob}\n"
+                                f"Uniform Crossover Probability: {uniform_crossover}\n"
+                                f"Cross Method: {cross_method}\n"
+                                f"Mutation Method: {mutation_method}\n"
+                                f"Function to Optimize: {function}\n"
+                                f"Optimization Mode: {optimization_mode}\n"
+                                f"Execution Time: {execution_time:.4f} seconds\n"
+                                f"Result: {result}\n")
